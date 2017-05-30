d7052b5cf4a8036b44f3032eaf5b353d3eb5d689
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2017-05-30 11:36:25 +0200

    category cleanup

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -82,7 +82,7 @@
  	/* translators: used between list items, there is a space after the comma */
  	$categories_list = get_the_category_list( esc_html__( ', ', 'kuhn' ) );
  	if ( $categories_list && kuhn_categorized_blog() ) {
- 		printf( '<span class="cat-links">' . esc_html__( '%1$s', 'kuhn' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+ 		printf( '<div class="cat-links">' . esc_html__( '%1$s', 'kuhn' ) . '</div>', $categories_list ); // WPCS: XSS OK.
  	}
  }
 
