006536902a19ac89fd8752a543c8405ac68f83d3
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2017-08-10 14:57:46 -0700

    Content images now responsive

--- functions.php
+++ functions.php
@@ -145,6 +145,70 @@ function kuhn_resource_hints( $urls, $relation_type ) {
 }
 add_filter( 'wp_resource_hints', 'kuhn_resource_hints', 10, 2 );
 
+
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for content images.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $sizes A source size value for use in a 'sizes' attribute.
+ * @param array  $size  Image size. Accepts an array of width and height
+ *                      values in pixels (in that order).
+ * @return string A source size value for use in a content image 'sizes' attribute.
+ */
+function kuhn_content_image_sizes_attr( $sizes, $size ) {
+	if ( is_singular() ) {
+		$width = $size[0];
+		if ( 720 <= $width ) {
+			$sizes = '(min-width: 990px) 720px, (min-width: 1300px) 610px, 95vw';
+		}
+		return $sizes;
+	}
+}
+add_filter( 'wp_calculate_image_sizes', 'kuhn_content_image_sizes_attr', 10, 2 );
+
+/**
+ * Filter the `sizes` value in the header image markup.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $html   The HTML image tag markup being filtered.
+ * @param object $header The custom header object returned by 'get_custom_header()'.
+ * @param array  $attr   Array of the attributes for the image tag.
+ * @return string The filtered header image HTML.
+ */
+function kuhn_header_image_tag( $html, $header, $attr ) {
+	if ( isset( $attr['sizes'] ) ) {
+		$html = str_replace( $attr['sizes'], '100vw', $html );
+	}
+	return $html;
+}
+add_filter( 'get_header_image_tag', 'kuhn_header_image_tag', 10, 3 );
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for post thumbnails.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param array $attr       Attributes for the image markup.
+ * @param int   $attachment Image attachment ID.
+ * @param array $size       Registered image size or flat array of height and width dimensions.
+ * @return string A source size value for use in a post thumbnail 'sizes' attribute.
+ */
+function kuhn_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+	if ( !is_singular() ) {
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			$attr['sizes'] = '(max-width: 900px) 90vw, 800px';
+		} else {
+			$attr['sizes'] = '(max-width: 1000px) 90vw, 1000px';
+		}
+	}
+	return $attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'kuhn_post_thumbnail_sizes_attr', 10, 3 );
+
 /**
  * Register widget area.
  *
